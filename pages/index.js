1e3395c83b6ec2d9ebd1d14b4ebbdbd155afff9c
Nate Naranjo <naranjo_nathaniel@live.com>
2023-01-16 22:33:18 -0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Head from 'next/head';
-import Link from 'next/Link';
 
 import Form from '../components/Form/Form';
 import { FormProvider } from '../context/FormContext';

d0bfe8f04daac164bcf0cce33bcf1769f6143d5e
Rez the Dev <natenaranjodev@gmail.com>
2023-01-17 04:22:19 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,25 @@
-import React from 'react';
-import Head from 'next/head';
-import Link from 'next/Link';
+import React from "react";
+import Head from "next/head";
 
-import Form from '../components/Form/Form';
-import { FormProvider } from '../context/FormContext';
+import Form from "../components/Form/Form";
+import { FormProvider } from "../context/FormContext";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>W3pedia</title>
-        <meta property='og:title' content='My page title' key='title' />
+        <meta property="og:title" content="My page title" key="title" />
       </Head>
 
-
-      <main className='flex flex-col lg:flex-row justify-center gap-8 p-2'>
-        <section className='w-full border-2 border-solid rounded-xl border-white hidden xl:block'>
-          <div className=''>
+      <main className="flex flex-col lg:flex-row justify-center gap-8 p-2">
+        <section className="w-full border-2 border-solid rounded-xl border-white hidden xl:block">
+          <div className="">
             <h1>Just a placeholder!</h1>
           </div>
         </section>
-        <section  className='w-full border-2 border-solid rounded-xl border-white'>
-        <div className=''>
+        <section className="w-full border-2 border-solid rounded-xl border-white">
+          <div className="">
             <FormProvider>
               <Form />
             </FormProvider>
@@ -29,5 +27,5 @@ export default function Home() {
         </section>
       </main>
     </>
-  )
+  );
 }
